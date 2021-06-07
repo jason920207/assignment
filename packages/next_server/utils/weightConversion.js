7eca0dca3d11c265c9afa7058e263e7a93d4be6c
xiaojie zhang <xiaojiezhang@xiaojiedeMacBook-Air.local>
2021-06-07 19:28:23 -0400

    fix style

--- packages/next_server/utils/weightConversion.js
+++ packages/next_server/utils/weightConversion.js
@@ -1,6 +1,4 @@
 export function weightFormat(weight, packageWeight) {
-
-
     let weightValue = weight ? weight : packageWeight
     weightValue = weightValue.match(/[0-9]+|g|lbs/g)
     return weightValue.join(' ')
